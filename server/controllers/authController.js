5303d37fc82337a947db5daecb1a9f5de98d9805
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-20 18:58:43 +0300

    Login CRUD operation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,6 +13,26 @@ exports.register = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created!", user: newUser });
   } catch (err) {
-    res.status(500).send("Something went wrong!");
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    const isPasswordCorrect = bcrypt.compare(req.body.password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Wrong password!" });
+    }
+
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong!" });
   }
 };
