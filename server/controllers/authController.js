0894c3604d7c2e53e5f7bb91771f2ca4d8280dc7
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-20 17:18:48 +0300

    Added password encryption

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,14 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
 
     await newUser.save();
     res.status(201).json({ message: "User created!", user: newUser });
