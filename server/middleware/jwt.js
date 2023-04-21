85e998c7ce1c35f29e3880a29cba6b2146bc73bd
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-22 01:14:41 +0300

    Utilized error handling middleware/function in jwt middleware

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
+const { createError } = require("../utils/createError");
 
-exports.verifyToken = (req, res) => {
+exports.verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not authenticated!");
+  if (!token) return next(createError(401, "You are not authenticated!"));
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (err) return res.status(403).send("Token is not valid!");
+    if (err) return next(createError(403, "Token is not valid!"));
 
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
