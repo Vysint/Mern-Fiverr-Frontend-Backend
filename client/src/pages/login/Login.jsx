4bfdb6ecd84a39bb2abfedecec514bbcd703baa9
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-22 15:03:42 +0300

    Added Login Page and it's CSS styling

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-import './Login.scss'
+import "./Login.scss";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="John Doe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
