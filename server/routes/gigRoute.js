f657d1889b8f1bf7f61f16e6ea9cec0d345b628e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-26 09:59:33 +0300

    Added CRUD operations for gigs

---
+++ server/routes/gigRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { verifyToken } = require("../middleware/jwt");
+const {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs,
+} = require("../controllers/gigController");
+
+const router = express.Router();
+
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", verifyToken, getGig);
+router.get("/", verifyToken, getGigs);
+
+module.exports = router;
