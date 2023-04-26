628642b2e602acafc65cbb798523d7b34b898c7c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-26 13:38:47 +0300

    Added GET user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { deleteUser } = require("../controllers/userController");
+const { deleteUser, getUser } = require("../controllers/userController");
 const { verifyToken } = require("../middleware/jwt");
 
 const router = express.Router();
 
 router.delete("/:id", verifyToken, deleteUser);
+
+router.get("/:id", verifyToken, getUser);
 
 module.exports = router;
