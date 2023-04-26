afcf0b70b73b75544cc27c881bf14d11f582682d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-26 13:37:18 +0300

    Added a gGET request to retrieve a user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,3 +11,10 @@ exports.deleteUser = async (req, res, next) => {
   await User.findByIdAndDelete(req.params.id);
   res.status(200).send("Account deleted!");
 };
+
+exports.getUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return next(createError(404, "User not found!"));
+
+  res.status(200).json(user);
+};
