bee4b696d0430263434decd49882b15c278874c2
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-26 13:45:48 +0300

    Fetched Gigs from the database

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -1,22 +1,49 @@
-import React, { useState } from "react";
-import GigCard from "../../components/gigCard/GigCard";
-import { gigs } from "../../data";
-
+import React, { useEffect, useRef, useState } from "react";
 import "./Gigs.scss";
+import GigCard from "../../components/gigCard/GigCard";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import { useLocation } from "react-router-dom";
 
-const Gigs = () => {
-  const [open, setOpen] = useState(false);
+function Gigs() {
   const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+  const minRef = useRef();
+  const maxRef = useRef();
+
+  const { search } = useLocation();
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs"],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
+  console.log(data);
 
   const reSort = (type) => {
     setSort(type);
     setOpen(false);
   };
 
+  useEffect(() => {
+    refetch();
+  }, [sort]);
+
+  const apply = () => {
+    refetch();
+  };
+
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN > </span>
+        <span className="breadcrumbs">Fiverr > Graphics & Design ></span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Fiverr's AI artists
@@ -24,39 +51,38 @@ const Gigs = () => {
         <div className="menu">
           <div className="left">
             <span>Budget</span>
-            <input type="text" placeholder="Min" />
-            <input type="text" placeholder="Max" />
-            <button>Apply</button>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
           </div>
           <div className="right">
-            <span className="sortBy">SortBy</span>
+            <span className="sortBy">Sort by</span>
             <span className="sortType">
               {sort === "sales" ? "Best Selling" : "Newest"}
             </span>
-            <img
-              src="./img/down.png"
-              alt=""
-              onClick={() => setOpen((prev) => !prev)}
-            />
+            <img src="/down.png" alt="" onClick={() => setOpen(!open)} />
             {open && (
               <div className="rightMenu">
                 {sort === "sales" ? (
-                  <span onClick={() => reSort("createAt")}>Newest</span>
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
                 ) : (
                   <span onClick={() => reSort("sales")}>Best Selling</span>
                 )}
+                <span onClick={() => reSort("sales")}>Popular</span>
               </div>
             )}
           </div>
         </div>
         <div className="cards">
-          {gigs.map((gig) => (
-            <GigCard key={gig.id} item={gig} />
-          ))}
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong!"
+            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Gigs;
