23c5910fd50bad67c6e1c93f1ad28fe96b35470d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-04-28 22:03:27 +0300

    Routes for CRUD operations

---
+++ server/routes/reviewRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const { verifyToken } = require("../middleware/jwt");
+
+const {
+  createReview,
+  getReviews,
+  deleteReview,
+} = require("../controllers/reviewController");
+
+const router = express.Router();
+
+router.post("/", verifyToken, createReview);
+
+router.get("/:gigId", getReviews);
+
+router.delete("/:id", deleteReview);
+
+module.exports = router;
