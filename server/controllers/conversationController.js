3dc661290c8e4b676fa1d479e152b5eba7ddcd06
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-10 21:43:21 +0300

    Conversation CRUD operation

---
+++ server/controllers/conversationController.js
@@ -0,0 +1,52 @@
+const Conversation = require("../models/conversation.model");
+const { createError } = require("../utils/createError");
+
+exports.getConversations = async (req, res, next) => {
+  try {
+    const conversations = await Conversation.find(
+      req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
+    );
+    res.status(200).send(conversations);
+  } catch (err) {
+    next(err);
+  }
+};
+exports.createConversation = async (req, res, next) => {
+  const newConversation = new Conversation({
+    id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+    sellerId: req.isSeller ? req.userId : req.body.to,
+    buyerId: req.isSeller ? req.body.to : req.userId,
+    readBySeller: req.isSeller,
+    readByBuyer: !req.isSeller,
+  });
+
+  try {
+    const savedConversation = await newConversation.save();
+
+    res.status(201).send(savedConversation);
+  } catch (err) {
+    next(err);
+  }
+};
+exports.getSingleConversation = async (req, res, next) => {
+  try {
+    const conversation = await Conversation.findOne({ id: req.params.id });
+
+    if (!conversation) return next(createError(404, "Conversation not found!"));
+    res.status(200).send(conversation);
+  } catch (err) {
+    next(err);
+  }
+};
+exports.updateConversation = async (req, res, next) => {
+  try {
+    const updatedConversation = await Conversation.findOneAndUpdate(
+      {
+        id: req.params.id,
+      },
+      { $set: { readBySeller: req.isSeller, readByBuyer: !req.isSeller } },
+      { new: true }
+    );
+    res.status(200).send(updatedConversation);
+  } catch (err) {}
+};
