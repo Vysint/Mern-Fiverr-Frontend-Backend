51e1852eb4638e6bdfdd53f55406f0e3f5edfd41
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-10 21:31:12 +0300

    Code cleanup

--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -21,60 +21,62 @@ const Messages = () => {
           <h1>Messages</h1>
         </div>
         <table>
-          <tr>
-            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Last Message</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-          <tr className="active">
-            <td>Charley Sharp</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 hour ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr className="active">
-            <td>John Doe</td>
+          <tbody>
+            <tr>
+              <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+              <th>Last Message</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+            <tr className="active">
+              <td>Charley Sharp</td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>1 hour ago</td>
+              <td>
+                <button>Mark as Read</button>
+              </td>
+            </tr>
+            <tr className="active">
+              <td>John Doe</td>
 
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 hours ago</td>
-            <td>
-              <button>Mark as Read</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Elinor Good</td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>1 day ago</td>
-          </tr>
-          <tr>
-            <td>Garner David </td>
-            <td>
-              <Link to="/message/123" className="link">
-                {message.substring(0, 100)}...
-              </Link>
-            </td>
-            <td>2 days ago</td>
-          </tr>
-          <tr>
-            <td>Troy Oliver</td>
-            <td>{message.substring(0, 100)}</td>
-            <td>1 week ago</td>
-          </tr>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>2 hours ago</td>
+              <td>
+                <button>Mark as Read</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Elinor Good</td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>1 day ago</td>
+            </tr>
+            <tr>
+              <td>Garner David </td>
+              <td>
+                <Link to="/message/123" className="link">
+                  {message.substring(0, 100)}...
+                </Link>
+              </td>
+              <td>2 days ago</td>
+            </tr>
+            <tr>
+              <td>Troy Oliver</td>
+              <td>{message.substring(0, 100)}</td>
+              <td>1 week ago</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
