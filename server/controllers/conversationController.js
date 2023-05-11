773c8f0b2cffd4282ef3b9cb5f23125dc744f9ce
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-12 00:50:54 +0300

    Code correction

--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -5,7 +5,7 @@ exports.getConversations = async (req, res, next) => {
   try {
     const conversations = await Conversation.find(
       req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
-    );
+    ).sort({ updatedAt: -1 });
     res.status(200).send(conversations);
   } catch (err) {
     next(err);
