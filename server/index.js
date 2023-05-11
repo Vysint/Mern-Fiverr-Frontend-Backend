525015cc2b3c07ac23dc2dc34ac502c1ae4ae718
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-11 13:13:33 +0300

    Conversations routes

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
 app.use("/api/orders", orderRoute);
-// app.use("/api/conversations", conversationRoute);
+app.use("/api/conversations", conversationRoute);
 // app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
 
