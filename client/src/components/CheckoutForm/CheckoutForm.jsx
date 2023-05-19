e6fb0d6af709e71399c1fd180baf72c1c8b424f0
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-19 23:20:37 +0300

    Markup change

--- client/src/components/CheckoutForm/CheckoutForm.jsx
+++ client/src/components/CheckoutForm/CheckoutForm.jsx
@@ -5,7 +5,7 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
-import "./CheckoutForm.css";
+import "./CheckoutForm.scss";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
