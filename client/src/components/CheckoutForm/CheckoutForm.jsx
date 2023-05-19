667b42a2006cdbf60e3b7379d69e01aa41f6be79
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-19 22:16:54 +0300

    stripe checkout payment form

--- client/src/components/CheckoutForm/CheckoutForm.jsx
+++ client/src/components/CheckoutForm/CheckoutForm.jsx
@@ -49,8 +49,6 @@ const CheckoutForm = () => {
     e.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
       return;
     }
 
@@ -60,15 +58,10 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:5173/success",
+        return_url: `${window.location.origin}/success`,
       },
     });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
     if (error.type === "card_error" || error.type === "validation_error") {
       setMessage(error.message);
     } else {
