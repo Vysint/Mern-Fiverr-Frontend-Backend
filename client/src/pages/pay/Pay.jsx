592d757b1022884f04f7ce52c8c15d37ee038f72
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-19 12:12:35 +0300

    Stripe payment

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import newRequest from "../../utils/newRequest";
+import { useParams } from "react-router-dom";
+import CheckoutForm from "../../components/CheckoutForm/CheckoutForm";
 import "./Pay.scss";
 
+const stripePromise = loadStripe(
+  "pk_test_51MAsgYKgCZTtTMfMtl1u0Ckj24xLIokMsuGx3jFaQX37WYKjr2uqnMIWHC0bS6SJbURd2BTv27X5jkwyczZy8M9D00ZBxmZuvZ"
+);
+
 const Pay = () => {
-  return <div>Pay</div>;
+  const [clientSecret, setClientSecret] = useState("");
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await newRequest.post(
+          `/orders/create-payment-intent/${id}`
+        );
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="pay">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
 };
 
 export default Pay;
