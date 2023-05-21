f76f0215de40aab6d3bd69aeb34d059c494d030c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-21 13:19:20 +0300

    Implemented gigs  search

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,28 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Featured.scss";
 
-const Featured = () => {
+function Featured() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/gigs?search=${input}`);
+  };
   return (
     <div className="featured">
       <div className="container">
         <div className="left">
           <h1>
-            Find the perfect <i>freelance</i> services for your business
+            Find the perfect <span>freelance</span> services for your business
           </h1>
           <div className="search">
             <div className="searchInput">
               <img src="/search.png" alt="" />
-              <input type="text" placeholder='Try "building mobile app"' />
+              <input
+                type="text"
+                placeholder='Try "building mobil app"'
+                onChange={(e) => setInput(e.target.value)}
+              />
             </div>
-            <button>Search</button>
+            <button onClick={handleSubmit}>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
             <button>Web Design</button>
-            <button>Wordpress</button>
+            <button>WordPress</button>
             <button>Logo Design</button>
-            <button>AI services</button>
+            <button>AI Services</button>
           </div>
         </div>
         <div className="right">
@@ -31,6 +41,6 @@ const Featured = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Featured;
