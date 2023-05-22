293ade565fde4b9f557140140d5a258cdcdecfbb
Vincent Kipngetich <vinniebluz@icloud.com>
2023-05-23 01:24:13 +0300

    Bug fix

--- client/src/reducers/gigReducer.js
+++ client/src/reducers/gigReducer.js
@@ -1,5 +1,5 @@
 export const INITIAL_STATE = {
-  userId: JSON.parse(localStorage.get("currentUser"))?._id,
+  userId: JSON.parse(localStorage.getItem("currentUser"))?._id,
   title: "",
   cat: "",
   cover: "",
